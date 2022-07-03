e392f16a5a44f017f6357b6440920181d20e47e4
root <root@ansible-cn.k8s.lab>
2022-07-03 20:53:50 +0530

    Made some changes in the website code

--- uploadfile.php
+++ uploadfile.php
@@ -1,3 +1,17 @@
-<?php $ path = "files/"; $path = $path . basename( $_FILES['userfile']['name']);
-
-if(move_uploaded_file($_FILES['userfile']['tmp_name'], $path)) { echo "Success uploading". basename($_FILES['userfile']['name']); } else{ echo "Error when uploading file."; } ?>
+<?php
+$target_dir = "/uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+if($check !== false) {
+echo "File is an image - " . $check["mime"] . ".";
+$uploadOk = 1;
+} else {
+echo "File is not an image.";
+$uploadOk = 0;
+}
+}
+?>
